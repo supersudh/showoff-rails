a108bf9b127a860433a0b42e79afc434b8cb8ce6
Sudharsan RaviKumar <supersudh@gmail.com>
2020-03-15 11:43:14 +0530

    heroku setup

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require_relative "boot"
 require "action_controller/railtie"
 require "active_storage/engine"
 require "action_mailer/railtie"
+require "action_cable/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
