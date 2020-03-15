664972899222807ac26abf7b6bd76887a769c06e
Sudharsan RaviKumar <supersudh@gmail.com>
2020-03-15 18:28:08 +0530

    Authentication and User APIs Added 1. Add createUser API 2. Add loginUser API 3. Add logout API 4. Add changePassword API 5. Add reset_password API 6. Add updateUser API 7. Add currentUser API 8. Update lib/showoff_api.rb to accomodate these new APIs 9. Modify routes.rb to add new routes for these new APIs

---
+++ app/controllers/v1/users_controller.rb
@@ -0,0 +1,89 @@
+class V1::UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def me
+    authorizationHeader = request.headers["HTTP_AUTHORIZATION"]
+    response = ShowoffApi.currentUser(authorizationHeader)
+    render json: response
+  end
+
+  def register
+    createUserData = { :user => register_user_params }
+    response = ShowoffApi.createUser(createUserData)
+    render json: response
+  end
+
+  def login
+    loginData = login_params
+    response = ShowoffApi.loginUser(loginData)
+    render json: response
+  end
+
+  def logout
+    token = logout_params[:token]
+    response = ShowoffApi.logout(token)
+    render json: response
+  end
+
+  def change_password
+    token = change_password_params[:token]
+    data = {
+      :user => {
+        :current_password => change_password_params[:current_password],
+        :new_password => change_password_params[:new_password],
+      },
+    }
+    response = ShowoffApi.changePassword(token, data)
+    render json: response
+  end
+
+  def reset_password
+    data = {
+      :user => {
+        :email => reset_password_params[:email],
+      },
+    }
+    response = ShowoffApi.reset_password(data)
+    render json: response
+  end
+
+  def update
+    token = update_user_params[:token]
+    puts token
+    data = {
+      :user => {
+        :first_name => update_user_params[:first_name],
+        :last_name => update_user_params[:last_name],
+        :date_of_birth => update_user_params[:date_of_birth],
+      },
+    }
+    response = ShowoffApi.updateUser(token, data)
+    render json: response
+  end
+
+  private
+
+  def register_user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :image_url)
+  end
+
+  def login_params
+    params.require(:user).permit(:username, :password)
+  end
+
+  def logout_params
+    params.require(:user).permit(:token)
+  end
+
+  def change_password_params
+    params.require(:user).permit(:token, :current_password, :new_password)
+  end
+
+  def reset_password_params
+    params.require(:user).permit(:email)
+  end
+
+  def update_user_params
+    params.require(:user).permit(:first_name, :last_name, :date_of_birth, :image_url, :token)
+  end
+end
--- app/lib/showoff_api.rb
+++ app/lib/showoff_api.rb
@@ -4,15 +4,67 @@ module ShowoffApi
     return url
   end
 
+  def self.makeDataForPost(path = "")
+    url = "#{ENV["showoff_api_url"]}/#{path}"
+    spreadBody = { :client_id => ENV["client_id"], :client_secret => ENV["client_secret"] }
+    headers = { "Content-Type": "application/json" }
+    return { :url => url, :spreadBody => spreadBody, :headers => headers }
+  end
+
   def self.getWidgets
-    url = self.makeUrlForGet("widgets/visible")
+    url = self.makeUrlForGet("api/v1/widgets/visible")
     widgets = RestClient.get(url, headers = {})
     return JSON.parse(widgets)
   end
 
-  def self.searchWidgets(term)
-    url = self.makeUrlForGet("widgets/visible", "&term=#{term}")
+  def self.searchWidgets(term = "")
+    url = self.makeUrlForGet("api/v1/widgets/visible", "&term=#{term}")
     widgets = RestClient.get(url, headers = {})
     return JSON.parse(widgets)
+  end
+
+  def self.createUser(createUserData = {})
+    configData = self.makeDataForPost("api/v1/users")
+    body = { **configData[:spreadBody], **createUserData }
+    RestClient.post(configData[:url], body.to_json, configData[:headers]) { |response, request, result| return response }
+  end
+
+  def self.loginUser(loginData = {})
+    configData = self.makeDataForPost("oauth/token")
+    body = { **configData[:spreadBody], **loginData, :grant_type => "password" }
+    RestClient.post(configData[:url], body.to_json, configData[:headers]) { |response, request, result| return response }
+  end
+
+  def self.logout(token = "")
+    configData = self.makeDataForPost("oauth/revoke")
+    body = { :token => token }
+    headers = { **configData[:headers], "Authorization": "Bearer #{token}" }
+    RestClient.post(configData[:url], body.to_json, headers) { |response, request, result| return response }
+  end
+
+  def self.changePassword(token = "", data = {})
+    configData = self.makeDataForPost("api/v1/users/me/password")
+    body = data
+    headers = { **configData[:headers], "Authorization": "Bearer #{token}" }
+    RestClient.post(configData[:url], body.to_json, headers) { |response, request, result| return response }
+  end
+
+  def self.reset_password(data = {})
+    configData = self.makeDataForPost("api/v1/users/reset_password")
+    body = { **configData[:spreadBody], **data }
+    RestClient.post(configData[:url], body.to_json, configData[:headers]) { |response, request, result| return response }
+  end
+
+  def self.updateUser(token = "", updateData = {})
+    configData = self.makeDataForPost("api/v1/users/me")
+    body = updateData
+    headers = { **configData[:headers], "Authorization": "Bearer #{token}" }
+    RestClient.put(configData[:url], body.to_json, headers) { |response, request, result| return response }
+  end
+
+  def self.currentUser(authorizationHeader = "")
+    url = self.makeUrlForGet("api/v1/users/me")
+    headers = { "Authorization": authorizationHeader }
+    currentUser = RestClient.get(url, headers) { |response, request, result| return response }
   end
 end
