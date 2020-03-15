620562c5cd5f4382411e83e054bf1d2496dad1af
Sudharsan RaviKumar <supersudh@gmail.com>
2020-03-15 11:22:46 +0530

    heroku setup

--- config/application.rb
+++ config/application.rb
@@ -16,11 +16,13 @@ module ShowoffRails
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.before_configuration do
-      env_file = File.join(Rails.root, "config", "secret.yml")
-      YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
-      end if File.exists?(env_file)
+    if ENV["RAILS_ENV"] != "production"
+      config.before_configuration do
+        env_file = File.join(Rails.root, "config", "secret.yml")
+        YAML.load(File.open(env_file)).each do |key, value|
+          ENV[key.to_s] = value
+        end if File.exists?(env_file)
+      end
     end
 
     config.autoload_paths << Rails.root.join("lib")
