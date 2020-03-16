de12feed1e3c830c1a398ca83a6e3c5aa002ac31
Sudharsan RaviKumar <supersudh@gmail.com>
2020-03-16 09:04:15 +0530

    Register Page 1. Redux actions,reducers, constants added 2. Register page using Redux form

--- app/javascript/containers/App/components/SignupForm.js
+++ app/javascript/containers/App/components/SignupForm.js
@@ -1,16 +1,82 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import { Field, reduxForm } from 'redux-form'
+import TextField from '@material-ui/core/TextField';
 
-export default class SignupForm extends Component {
-  static propTypes = {
-    // prop: PropTypes
+const validate = values => {
+  const errors = {}
+  const requiredFields = [
+    'first_name',
+    'last_name',
+    'email',
+    'password',
+  ]
+  requiredFields.forEach(field => {
+    if (!values[field]) {
+      errors[field] = 'Required'
+    }
+  })
+  if (
+    values.email &&
+    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+  ) {
+    errors.email = 'Invalid email address'
   }
+  return errors
+}
 
+const renderTextField = ({
+  input,
+  label,
+  meta: { touched, error },
+  ...custom
+}) => {
+  return (
+    <TextField
+      helperText={label}
+      label={error ? error : label}
+      error={touched && !!error}
+      {...input}
+      {...custom}
+      style={{ width: '100%' }}
+    />
+  );
+}
+
+class SignupForm extends React.Component {
   render() {
+    const { handleSubmit, pristine, submitting } = this.props
     return (
-      <div>
-        Signup Form
-      </div>
+      <form onSubmit={handleSubmit} style={{ width: '50%' }}>
+        <h3>Register</h3>
+        <div>
+          <Field
+            name="first_name"
+            component={renderTextField}
+            label="First Name"
+          />
+        </div>
+        <div>
+          <Field name="last_name" component={renderTextField} label="Last Name" />
+        </div>
+        <div>
+          <Field name="email" component={renderTextField} label="Email" />
+        </div>
+
+        <div>
+          <Field name="password" component={renderTextField} label="Password" type="password" />
+        </div>
+
+        <div>
+          <button type="submit" disabled={pristine || submitting}>
+            Submit
+          </button>
+        </div>
+      </form>
     );
   }
 }
+
+export default reduxForm({
+  form: 'SignupForm',
+  validate,
+})(SignupForm)
--- app/javascript/reducers/index.js
+++ app/javascript/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
 import globalReducer from './global';
 
+
 export default combineReducers({
+  form: formReducer,
   global: globalReducer
 });
